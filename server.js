881b17a08f3c4ca7a1e600a698fa624e21c5dfce
Saryn Caister <thegreenmajority@gmail.com>
2019-04-03 19:27:36 -0400

    my first push

--- server.js
+++ server.js
@@ -1,50 +1,47 @@
 var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var path = require("path");
-
-//____________________________________________________________________________________________________
+var exphbs = require("express-handlebars");
+var Handlebars = require("handlebars");
+var MomentHandler = require("handlebars.moment");
+MomentHandler.registerHelpers(Handlebars);
+var hbs = require('hbs');
+require('handlebars-form-helpers').register(hbs.handlebars);
+var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-var app = express();
+app.use(express.static("public"));
 
-// Configure middleware
-app.use(logger("dev")); // Use morgan logger for logging requests
-app.use(express.urlencoded({ extended: true })); // Parse request body as JSON
-app.use(express.json());
-app.use(express.static("public")); // Make public a static folder
-
-
-var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require("./routes/htmlRoutes.js")(app);
-var routes = require("./routes/apiRoutes.js");
-app.use(routes);
+var mysql = require('mysql');
 
-
-mongoose.set('useCreateIndex', true);
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mooddb";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-var dbConnection = mongoose.connection;
-dbConnection.on('error', console.error.bind(console, 'connection error:'));
-var collectionNames;
-dbConnection.once('open', function () {
-  console.log("MongoDB is connected");
-  dbConnection.db.listCollections().toArray((error, collections) => {
-    if (error) console.log(error);
-    collectionNames = collections.map((collection) => collection.name);
-    module.exports = { collectionNames }; //not of use right now but may become useful
-    //console.log(module.exports.collectionNames);
-  });
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "AzureHack"
 });
 
+con.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + con.threadId);
+});
 
+app.get("/", function (req, res) {
+    con.query("SELECT * FROM AzureHack.test1;", function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        res.render("index", {data: data});
+    });
+});
 
 app.listen(PORT, function () {
-  console.log("App running on port " + PORT + "!");
+    console.log("App listening on PORT: " + PORT);
 });
